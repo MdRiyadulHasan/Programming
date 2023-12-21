1cfb220944d30d283a77a5fd15c64d756735c527
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-12-21 21:34:19 +0600

    Create 1991. Find the Middle Index in Array.py

---
+++ Subarray/1991. Find the Middle Index in Array.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        leftSum = 0
+        for i, n in enumerate(nums):
+            if leftSum == total-leftSum-n:
+                return i
+            leftSum+=n 
+        return -1
+nums = [2,3,-1,8,4]
+ob = Solution()
+result = ob.findMiddleIndex(nums)
+print(result)
