e2906e3ed5f5bb85b53b890127f2bd58a836346c
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-12-21 22:21:07 +0600

    Create 1013. Partition Array Into Three Parts With Equal Sum.py

---
+++ Subarray/1013. Partition Array Into Three Parts With Equal Sum.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum/description/
+from typing import List
+class Solution:
+    def canThreePartsEqualSum(self, arr: List[int]) -> bool:
+        total_sum = sum(arr)
+
+        if total_sum % 3 != 0:
+            return False
+
+        target_sum = total_sum // 3
+        current_sum = 0
+        count = 0
+
+        for num in arr:
+            current_sum += num
+
+            if current_sum == target_sum:
+                count += 1
+                current_sum = 0
+
+        # Check if we found exactly 3 segments with equal sums
+        return count >= 3
+arr = [0,2,1,-6,6,-7,9,1,2,0,1]
+ob = Solution()
+result = ob.canThreePartsEqualSum(arr)
+print (result)
