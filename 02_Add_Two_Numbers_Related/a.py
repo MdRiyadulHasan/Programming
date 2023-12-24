7c0b7748117483026710d5635315a347d4892609
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-12-24 16:16:23 +0600

    Update a.py

--- 02_Add_Two_Numbers_Related/a.py
+++ 02_Add_Two_Numbers_Related/a.py
@@ -1,32 +1,26 @@
 from typing import List 
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        s = ''
-        for n in num:
-            s+=str(n)
-        ans = str(int(s)+k)
-        num = [n for n in ans]
-        return num
-class Solution:
-    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
         if not num:
-            return [int(d) for d in str(k)]
+           ans= [int(d) for d in str(k)]
+           return ans 
+        result = []
         carry = 0
-        res = []
-        for i in range(len(num)-1, -1, -1):
-            total = num[i] + carry + (k % 10)
-            carry = total // 10
-            res.append(total % 10)
-            k //= 10
-        while k > 0:
-            total = carry + (k % 10)
-            carry = total // 10
-            res.append(total % 10)
-            k //= 10
-        if carry > 0:
-            res.append(carry)
-        return res[::-1]
-        
+        for i in range(len(num)-1,-1,-1):
+           total = num[i]+carry+(k%10)
+           rem = total%10
+           carry = total//10
+           k = k//10
+           result.append(rem)
+        while k:
+            total = carry+(k%10)
+            rem = total%10
+            carry = total//10
+            k = k//10
+            result.append(rem)
+        if carry:
+            result.append(carry)
+        return result[::-1]     
 num = [2,7,4] 
 k = 181
 ob = Solution()
