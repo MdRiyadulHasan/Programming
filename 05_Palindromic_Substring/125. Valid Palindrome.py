7a771c123276ec4df1e0417ce6be7a1d400f6c07
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-12-27 19:39:39 +0600

    Create 125. Valid Palindrome.py

---
+++ 05_Palindromic_Substring/125. Valid Palindrome.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/valid-palindrome/
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l = l + 1
+            while l < r and not s[r].isalnum():
+                r = r-1
+            if s[l].lower()!=s[r].lower():
+                return False
+            l = l + 1
+            r = r-1
+        return True
+s = "A man, a plan, a canal: Panama"
+ob = Solution()
+result = ob.isPalindrome(s)
+print(result)
+        
