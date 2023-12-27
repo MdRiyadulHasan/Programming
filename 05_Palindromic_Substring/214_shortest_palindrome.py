5032fe46829daf4d9a7852168bfc486ba7a29bf2
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-12-27 16:35:35 +0600

    Update 214_shortest_palindrome.py

--- 05_Palindromic_Substring/214_shortest_palindrome.py
+++ 05_Palindromic_Substring/214_shortest_palindrome.py
@@ -26,4 +26,14 @@ def shortestPalindrome(s):
 # Example usage:
 s = "aacecaaa"
 result = shortestPalindrome(s)
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        length_s = len(s)
+        rep = ''
+        for i in range(length_s//2):
+            rep+=s[i]
+            if length_s%len(rep)==0:
+                if rep*(length_s//len(rep))==s:
+                    return True
+        return False
 print(result)
