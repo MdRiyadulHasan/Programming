8272ad445e1258c360719e8176d4ef83e90f1efe
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2024-01-05 19:47:48 +0600

    Update 80. Remove Duplicates from Sorted Array II.py

--- 26_Remove_Duplicates_From_Array/80. Remove Duplicates from Sorted Array II.py
+++ 26_Remove_Duplicates_From_Array/80. Remove Duplicates from Sorted Array II.py
@@ -3,9 +3,9 @@
 from typing import List
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        k = 3
-        for i in range(3, len(nums)):
-            if nums[i]!=nums[k-3]:
+        k = 2
+        for i in range(2, len(nums)):
+            if nums[i]!=nums[k-2]:
                 nums[k]=nums[i]
                 k=k+1
         return k
