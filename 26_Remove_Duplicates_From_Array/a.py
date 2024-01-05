94c0fb5a18b1cad84b49f7bb68b586b7aa9ebf7f
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2024-01-05 19:09:12 +0600

    Create a.py

---
+++ 26_Remove_Duplicates_From_Array/a.py
@@ -0,0 +1,13 @@
+from typing import List 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        start = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[start], nums[i] = nums[i],nums[start]
+                start+=1
+        return nums 
+ob = Solution()
+nums = [0,1,0,3,12]
+result = ob.moveZeroes(nums)
+print(result)
