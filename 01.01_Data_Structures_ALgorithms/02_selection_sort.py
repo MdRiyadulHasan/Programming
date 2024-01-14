b0247ffa86e1abccfe76da4744cc95be391d08f2
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2024-01-14 11:58:34 +0600

    Create 02_selection_sort.py

---
+++ 01.01_Data_Structures_ALgorithms/02_selection_sort.py
@@ -0,0 +1,8 @@
+def SelectionSort(nums):
+    for i in range(len(nums)-1):
+        min = i 
+        for j in range(min+1, )
+if __name__ == '__main__':
+    nums = [30,15,20,25,12,8,40,22,17]
+    result = SelectionSort(nums)
+    print(result)
