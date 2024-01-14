e3f53df11143f9470320f28bb7b4a9915824d308
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2024-01-14 12:06:00 +0600

    Update 02_selection_sort.py

--- 01.01_Data_Structures_ALgorithms/02_selection_sort.py
+++ 01.01_Data_Structures_ALgorithms/02_selection_sort.py
@@ -1,7 +1,13 @@
 def SelectionSort(nums):
     for i in range(len(nums)-1):
         min = i 
-        for j in range(min+1, )
+        for j in range(min+1, len(nums)):
+            if nums[j]<nums[min]:
+                min = j
+        if min!=i:
+            nums[min],nums[i]= nums[i], nums[min]
+    return nums
+
 if __name__ == '__main__':
     nums = [30,15,20,25,12,8,40,22,17]
     result = SelectionSort(nums)
