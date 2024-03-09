9194420bb55d95ef3b0252553e26f59f751688f6
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2024-03-09 23:33:00 +0600

    Create leetcode_560.py

---
+++ Practice/leetcode_560.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/subarray-sum-equals-k/
+from typing import List
+# {0: 1, 3: 1, 7: 1, 14: 2, 16: 1, 13: 1, 18: 1, 20: 1, 21: 1}
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        dic = {0:1}
+        count = 0
+        cumulativeSum = 0
+        for n in nums:
+            cumulativeSum+=n
+            diff = cumulativeSum-k
+            if diff in dic:
+               count+=dic[diff]
+            dic[cumulativeSum]=dic.get(cumulativeSum,0)+1
+        return count 
+nums= [3, 4, 7, 2, -3, 1, 4, 2,1]
+k= 7
+solution_instance = Solution()
+result = solution_instance.subarraySum(nums, k)
+print(result)
+
+
