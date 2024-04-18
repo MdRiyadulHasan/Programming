c25c3f5b89c9be258882ff7de66fd053a0cf9d6f
MdRiyadulHasan <riadulhasancse48@gmail.com>
2024-04-18 09:17:41 +0600

    Create a.py

---
+++ 01_Two_Sum_And_Others/a.py
@@ -0,0 +1,21 @@
+from typing import List 
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        count = 0
+        for i in range (len(nums)-1, 1, -1):
+            l=0
+            r = i-1
+          
+            while l<r:
+                
+                if nums[l]+nums[r]>nums[i]:
+                    count = count + r-l
+                    r = r-1
+                else:
+                    l = l+1
+        return count
+ob = Solution()
+nums = [4,2,3,4]
+res = ob.triangleNumber(nums)
+print("res", res)
