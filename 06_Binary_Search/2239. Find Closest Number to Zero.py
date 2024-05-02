a06f534d9131ab859a993e78cd90c5aa020c6fdf
MdRiyadulHasan <riadulhasancse48@gmail.com>
2024-05-03 00:55:40 +0600

    Binary Search

---
+++ 06_Binary_Search/2239. Find Closest Number to Zero.py
@@ -0,0 +1,5 @@
+from typing import List 
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        val = min([abs(i) for i in nums])
+        return val if val in nums else -val
