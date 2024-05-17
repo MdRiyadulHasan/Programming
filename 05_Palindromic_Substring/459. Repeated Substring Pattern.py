a1efd9644cc43cbe13216b3d61e536208d0fad8c
MdRiyadulHasan <riadulhasancse48@gmail.com>
2024-05-18 00:37:32 +0600

    Leetcode 17-05-2024

--- 05_Palindromic_Substring/459. Repeated Substring Pattern.py
+++ 05_Palindromic_Substring/459. Repeated Substring Pattern.py
@@ -1,14 +1,14 @@
 # https://leetcode.com/problems/repeated-substring-pattern/submissions/
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        s_len = len(s)
-        rep = ""
-        for i in range(s_len//2):
-            rep +=s[i]
-            if s_len%len(rep) == 0:
-                if rep*(s_len//len(rep))==s:
+        n = len(s)  
+        substr = "" 
+        for i in range(n//2):
+            substr+=s[i]  
+            if n%len(substr)==0:
+                if substr*(n//len(substr))==s:
                     return True
-        return False     
+        return False   
 ob = Solution()
 s = "abcabcabcabc"
 ans = ob.repeatedSubstringPattern(s)
---
+++ 05_Palindromic_Substring/a.py
@@ -0,0 +1,16 @@
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        n, m = divmod(len(b), len(a))
+        if m:
+            n += 1
+        if b in a * n:
+            return n
+        elif b in a * (n+1):
+            return n+1
+        else:
+            return -1
+a = "abcd"
+b = "cdabcdab"
+ob = Solution() 
+res = ob.repeatedStringMatch(a,b)
+print(res)
