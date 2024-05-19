a97116623539cfc585eca5ccdbd89c5cefbf6b27
MdRiyadulHasan <riadulhasancse48@gmail.com>
2024-05-19 11:55:47 +0600

    Create 2810. Faulty Keyboard.py

---
+++ Strings & Array/2810. Faulty Keyboard.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/faulty-keyboard/description/ 
+
+class Solution:
+    def finalString(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            if s[i]=="i":
+                res = res[::-1]
+            else:
+                res+=s[i]
+        return res
+
+s = "poiinter"           
+
+
+        
