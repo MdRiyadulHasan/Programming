41455fb3d79195b4d8679fad3196292d3dafc239
MdRiyadulHasan <riadulhasancse48@gmail.com>
2024-05-24 20:16:30 +0600

    Create 53. Maximum Subarray.py

---
+++ Dynamic_Programming/53. Maximum Subarray.py
@@ -0,0 +1,13 @@
+from typing import List 
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = nums[0]
+        curSum = 0
+        for n in nums:
+            curSum +=n
+            maxSum = max(maxSum, curSum)
+            if curSum<0:
+                curSum = 0 
+        return maxSum
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]       
