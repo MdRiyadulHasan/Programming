81bb7c26d92f371755f61c6d34e9b494c9f89777
MdRiyadulHasan <riadulhasancse48@gmail.com>
2024-06-29 18:58:29 +0600

    Bit Manipulation

---
+++ 02_Bit_Manipulation/187. Repeated DNA Sequences.py
@@ -0,0 +1,11 @@
+from typing import List 
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        seen, res = set(), set()
+        for i in range(len(s)-9):
+            cur = s[i:i+10] 
+            if cur in seen:
+                res.add(cur)
+            seen.add(cur)
+        return list(res)
+        
